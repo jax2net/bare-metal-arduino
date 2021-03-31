054604bdc137b1ee9d8b5a145d76b5779791dab9
jsns <jsns@disroot.org>
2021-03-30 19:55:14 -0700

    count from 1 to 3 on one digit, seven segment display

--- src/main.c
+++ src/main.c
@@ -2,16 +2,64 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-// change
+#define DELAY_MS 1000
+
+void init() {
+	DDRD |= (1<<PD2);
+	DDRD |= (1<<PD3);
+	DDRD |= (1<<PD4);
+	DDRD |= (1<<PD5);
+	DDRD |= (1<<PD6);
+	DDRD |= (1<<PD7);
+	DDRB |= (1<<PB0);
+}
+
+/* clear all bits (set bits to 0) */
+void clear() {
+	PORTD &= ~(1<<PD2);
+	PORTD &= ~(1<<PD3);
+	PORTD &= ~(1<<PD4);
+	PORTD &= ~(1<<PD5);
+	PORTD &= ~(1<<PD6);
+	PORTD &= ~(1<<PD7);
+	PORTB &= ~(1<<PB0);
+}
+
+void one(void) {
+	PORTD |= (1<<PD4); // bottom right
+	PORTB |= (1<<PB0); // top right
+}
+
+void two(void) {
+	PORTD |= (1<<PD7); // top
+	PORTB |= (1<<PB0); // top right
+	PORTD |= (1<<PD5); // middle
+	PORTD |= (1<<PD2); // bottom left
+	PORTD |= (1<<PD3); // bottom
+}
+
+void three(void) {
+	PORTD |= (1<<PD7);
+	PORTD |= (1<<PD4);
+	PORTD |= (1<<PD5);
+	PORTB |= (1<<PB0);
+	PORTD |= (1<<PD3);
+}
+
+
 int main(void) {
-	DDRB |= (1<<PB5);
-	DDRD &= ~(1<<PD2); // input mode on PIND2
 	while (1) {
-		if(PIND & (1<<2)) {
-			PORTB |= (1<<PB5);
-		} else {
-			PORTB &= ~(1<<PB5);
-		}
+		one();
+		_delay_ms(DELAY_MS);
+		clear();
+
+		two();
+		_delay_ms(DELAY_MS);
+		clear();
+
+		three();
+		_delay_ms(DELAY_MS);
+		clear();
 	}
 	return 0;
 }
